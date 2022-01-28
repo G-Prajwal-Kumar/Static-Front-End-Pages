bb8e2c8cc16200f526b75445aa99e10f872df8d6
Prajwal Kumar <prajwalg2001@gmail.com>
2022-01-28 20:34:17 +0530

    this

--- weather/weather.js
+++ weather/weather.js
@@ -24,7 +24,7 @@ request.onload = function() {
         document.getElementById('humid').innerHTML= "Humidity : "+data.main.humidity+"%";
         document.getElementById('wind').innerHTML= "Wind Speed : "+data.wind.speed;
         document.getElementById('myimg').src=imgsrc;
-        /*document.getElementById('bdy').style.background-image = "url(C:/Users/prajwal/Downloads/"+city+".jpg);"*/
+        document.body.style.backgroundImage = "url("+city+".jpg)"
 
     }
     else {
